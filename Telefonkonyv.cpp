b18f39941db3ad2198f677f1126b109e34f98806
Mészáros László <meszaros.laszlo2000@edu.bme.hu>
2021-05-07 14:23:33 +0200

    Telefonkonyv.cpp-be átírva

--- Telefonkonyv.cpp
+++ Telefonkonyv.cpp
@@ -1,1 +1,80 @@
-//asd
+#include "Telefonkonyv.h"
+
+
+/*void Telefonkonyv::torol(Bejegyzes& torlendo) {
+		if(*elso == torlendo)
+
+
+
+		akt = elso;
+		Bejegyzes* lemarado = elso;
+		while (!(*akt == torlendo)) {
+			akt = akt->getKov();
+		}
+
+	}*/
+
+void Telefonkonyv::hozzaad(Bejegyzes const& hozzaadando){
+	while (akt != NULL)
+	{
+		akt = akt->getKov();
+	}
+	*akt = hozzaadando;
+}
+
+void Telefonkonyv::kiir(std::ostream& os) const
+{
+	Bejegyzes* mozgo = elso;
+	while (mozgo != NULL)
+	{
+		mozgo->kiir(os);
+		os << '\n';
+	}
+}
+
+
+void Telefonkonyv::beolvas(std::istream& is)
+{
+	//Bejegyzes temp;
+	//akt = &temp;
+	char c;
+	akt->beolvas(is);
+	akt = akt->getKov();
+	is >> c;
+	elso = akt;
+	while (c != EOF && c == '\n')
+	{
+		akt->beolvas(is);
+		akt = akt->getKov();
+		is >> c;
+	}
+	akt->setKov(NULL);
+}
+
+
+
+
+Bejegyzes* Telefonkonyv::find(const String keresendo)
+{
+	Bejegyzes* tmp = elso;
+	while (tmp != nullptr)
+	{
+		if (tmp->find(keresendo))
+			return tmp;
+	}
+}
+
+
+//megkeresi az összes találatot a telefonkönyben és egy újba fûzi
+	//lehet használni rajta a kiíratást
+Telefonkonyv Telefonkonyv::findAll(const String keresendo)
+{
+	Bejegyzes* tmp = elso;
+	Telefonkonyv result;
+	while (tmp != nullptr)
+	{
+		if (tmp->find(keresendo))
+			result.hozzaad(*tmp);
+	}
+	return result;
+}
--- Telefonkonyv.h
+++ Telefonkonyv.h
@@ -37,35 +37,18 @@ public:
 	};
 
 	///Kitorli a torlendo Bejegyzest a Telefonkonyvbol
-	///**Felbehagytam**
-	void torol(Bejegyzes &torlendo); /* {
-		if(*elso == torlendo)
+	void torol(Bejegyzes &torlendo);
 		
-		
-		
-		akt = elso;
-		Bejegyzes* lemarado = elso;
-		while (!(*akt == torlendo)) {
-			akt = akt->getKov();
-		}
-
-	}*/
 
 	///Hozzaadja a hozzaadando Bejegyzest a Telefonkonyvbe
-	void hozzaad(Bejegyzes const &hozzaadando)
-	{
-		while (akt != NULL)
-		{
-			akt = akt->getKov();
-		}
-		*akt = hozzaadando;
-	}
+	void hozzaad(Bejegyzes const& hozzaadando);
+	
 
 	///Kikeres egy Bejegyzest a Telefonkonyvbol egy megadott adat alapjan
-
-	template <class Funktor>
-	Bejegyzes *kereses(String const &adat, Funktor fun)
-	{
+	//Nem jo???
+	//template <class Funktor>
+	//Bejegyzes* kereses(String const& adat, Funktor fun);
+	/*{
 		akt = elso;
 		while (akt != NULL)
 		{
@@ -73,60 +56,21 @@ public:
 			akt = akt->getKov();
 		}
 		return NULL;
-	}
+	}*/
 
-	void kiir(std::ostream &os) const
-	{
-		Bejegyzes *mozgo = elso;
-		while (mozgo != NULL)
-		{
-			mozgo->kiir(os);
-			os << '\n';
-		}
-	}
+	void kiir(std::ostream& os) const;
 
-	void beolvas(std::istream &is)
-	{
-		//Bejegyzes temp;
-		//akt = &temp;
-		char c;
-		akt->beolvas(is);
-		akt = akt->getKov();
-		is >> c;
-		elso = akt;
-		while (c != EOF && c == '\n')
-		{
-			akt->beolvas(is);
-			akt = akt->getKov();
-			is >> c;
-		}
-		akt->setKov(NULL);
-	}
 
-	// megkeresi az első találatot a atelefonkönyvben
-	Bejegyzes *find(const String keresendo)
-	{
-		Bejegyzes *tmp = elso;
-		while (tmp != nullptr)
-		{
-			if (tmp->find(keresendo))
-				return tmp;
-		}
-	}
+	void beolvas(std::istream& is);
+
+	// megkeresi az első találatot a telefonkönyvben
+	Bejegyzes* find(const String keresendo);
+
 
 	//megkeresi az összes találatot a telefonkönyben és egy újba fűzi
 	//lehet használni rajta a kiíratást
-	Telefonkonyv findAll(const String keresendo)
-	{
-		Bejegyzes *tmp = elso;
-		Telefonkonyv result;
-		while (tmp != nullptr)
-		{
-			if (tmp->find(keresendo))
-				result.hozzaad(*tmp);
-		}
-		return result;
-	}
+	Telefonkonyv findAll(const String keresendo);
+
 };
 
 #endif
